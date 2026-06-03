1eecbc0c23b79a5f8b0fc1e3db11d8cf7ff6a777
jeferson46pinilla-jpg <jeferson46pinilla@gmail.com>
2026-06-03 17:44:23 +0000

    clase 28 pantalla de candidatos

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const btnMensaje = document.getElementById('btnMensaje');
 const mensajeClase = document.getElementById('mensajeClase');
+const botonesPerfil = document.querySelectorAll('.btnPerfil');
+const detallePerfil = document.getElementById('detallePerfil');
 
-let contadordeclics = 1;
+let contadordeclics = 0;
 
 btnMensaje.addEventListener('click', () => {
-  //  mensajeClase.textContent = "¡Hola! Este es un mensaje de alerta.";
+   // mensajeClase.textContent = "¡Hola! Este es un mensaje de alerta.";
     mensajeClase.textContent = "el texto que quisimos mostrar" + contadordeclics;
     contadordeclics = contadordeclics + 1;
 
     
 });
+
+botonesPerfil.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    const perfil = boton.getAttribute('data-perfil');
+    // Changed "Perfil" to "perfil" to match the variable declaration
+    detallePerfil.textContent = `informacion detallada del perfil: ${perfil}`;
+  });
+});
+  
